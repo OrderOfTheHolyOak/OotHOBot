5d060a3fc073d8e6345ce7582f9c940a169f037d
Michael Stokoe <mstokoe0990@gmail.com>
2021-08-05 14:32:31 +0100

    Made wiki bot nicer

--- oothobot/cogs/wiki.py
+++ oothobot/cogs/wiki.py
@@ -9,14 +9,15 @@ class wiki(commands.Cog):
 
   @commands.command()
   async def wiki(self, ctx, *, search : str):
-    response = requests.get(f"https://newworld.fandom.com/wiki/{search.title().replace(' ', '_')}")
+    url = f"https://newworld.fandom.com/wiki/{search.title().replace(' ', '_')}"
+    msg = f"Found a page for {search}!\n{url}"
 
-    if response.status_code == 200:
-      url = f"https://newworld.fandom.com/wiki/{search.title().replace(' ', '_')}"
-    else:
+    response = requests.get(url)
+    if response.status_code != 200:
       url = f"https://newworld.fandom.com/wiki/Special:Search?query={quote_plus(search)}&scope=internal&navigationSearch=true"
+      msg = f"Didn't find a page for {search}\nBut here's a link to the wiki's search instead!\n{url}"
 
-    await ctx.send(f"You searched for \"{search}\"\n{url}")
+    await ctx.send(msg)
 
 def setup(bot):
   bot.add_cog(wiki(bot))
